47b187b2b6b372f28a95300dd5f4f22e9e3863df
Keymaster-hub <keymasterwow@gmail.com>
2020-01-08 00:15:55 +0200

    refactoring docs

--- main.py
+++ main.py
@@ -8,24 +8,20 @@ copyright = 'PythDOS, 2020. All rights reserved.'
 print(ver)
 print(copyright + '\n')
 
-functions = {'tree':tree,
-             'quit':quit, 'exit':quit, 'x':quit,
-             'dir':dir, 'ls':dir,
-             'cd':cd,
-             'help':help, '?':help
+functions = {'tree': tree,
+             'quit': quit, 'exit': quit, 'x': quit,
+             'dir': dir, 'ls': dir,
+             'cd': cd,
+             'help': help, '?': help
 
              }
 
-
-
 while True:
-    cli = []
-    cli = re.split(r'[/ >]', (input(os.getcwd() + '>').lower()))
-    if len(cli) > 1 and cli[1] == '?':
-        print('\n')
-        help(cli)
-        print('\n')
-    elif str(cli[0]) in functions:
-        print('\n')
-        functions[cli[0]](cli)
-        print('\n')
+    cli = re.split(r'[/ ]', (input(os.getcwd() + '>').lower()), maxsplit=1)
+    if str(cli[0]) in functions:
+        if len(cli) > 1 and cli[1] == '?':
+            print(functions[cli[0]].__doc__)
+        else:   
+            print('\n')
+            functions[cli[0]](cli)
+            print('\n')
