cd5c8f9c4a5e25f4376aed014653f57f85bf3579
unknown <D:\The Bat!>
2020-01-15 16:20:03 +0200

    refactoring stduot

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import utils
 import os
 import re
-
+from contextlib import redirect_stdout
 
 ver = 'PythDOS [Version 0.0.3123]'
 copyright = 'PythDOS, 2020. All rights reserved.'
@@ -24,9 +24,14 @@ while True:
     argument = cli[2]
     if function_name in functions and argument == '/?':
         print(functions[function_name].__doc__)
+    elif function_name in functions and (len(argument) and argument[0] == '>'):
+        with open(argument[1:], 'w') as file:
+            with redirect_stdout(file):
+                functions[function_name]('')    
     elif function_name in functions:
         functions[function_name]((argument).strip())
         print('\n')
     elif function_name != ' ' or not function_name.isalpha(): 
         print(function_name + argument, """ не является внутренней или внешней
 командой, исполняемой программой или пакетным файлом.""")
+
